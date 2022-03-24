755a3bdab3a75acb6b6c8e0cad15cad6179d200c
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-24 15:15:26 -0300

    feat: loopings

--- project/app/controllers/application.js
+++ project/app/controllers/application.js
@@ -4,4 +4,13 @@ export default Controller.extend({
   firstName: 'Luis',
   lastName: 'Carlos',
   canShow: false,
+  myList: [
+    { name: 'amabile', age: 22 },
+    { name: 'billy', age: 16 },
+    { name: 'caroline', age: 15 }
+  ],
+  myNumbers: {
+    odd: [1, 3, 5],
+    even: [2, 4, 6]
+  }
 });
