ec759c1e21903a45928d51ca77f875b2574d32e4
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-24 23:40:45 -0300

    feat: create services in project

--- pokedex/app/controllers/index.js
+++ pokedex/app/controllers/index.js
@@ -1,23 +1,12 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import { inject } from '@ember/service';
 
 export default Controller.extend({
   pkmFilter: '',
-  list: [
-    {
-      "name": 'pikachu',
-      "number": '025'
-    },
-    {
-      "name": 'chikorita',
-      "number": '152'
-    },
-    {
-      "name": 'blaziken',
-      "number": '257'
-    }
-  ],
-  pkmList: Ember.computed('list', 'pkmFilter', function () {
-    var pkmList = this.get('list'),
+  pokeService: inject(),
+  pkmList: Ember.computed('pokeService.list', 'pkmFilter', function () {
+    var pkmList = this.get('pokeService').get('list'),
       pkmFilter = this.get('pkmFilter').toLowerCase();
 
     return pkmList.filter(pkm => pkm.name.includes(pkmFilter));
