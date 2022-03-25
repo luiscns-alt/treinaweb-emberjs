0889c79fd64c2f6d5bf0d992ce52e778bc83ea9d
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-24 23:27:52 -0300

    feat: create services

--- project/app/controllers/products.js
+++ project/app/controllers/products.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   init() {
     this.queryParams = [{prodId: 'productId'}];
     this._super(...arguments)
   },
-  prodId: ''
+  prodId: '',
+  myService: service()
 });
